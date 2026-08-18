0c02b034d2b9855fa0f04aedb7da12e084334f15
Caro <fllcortizv@gmail.com>
2026-08-17 19:00:05 -0600

    Hola en tres líneas

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("¡Hola, Carolina!");
+Console.WriteLine("¡Hola, Carolina!");
+Console.WriteLine("¡Hola, Carolina!");
+
+Console.Write("Hola");
+Console.Write("Carolina");
+Console.Write("!");
+
+
+
+
