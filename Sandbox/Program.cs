5c130359a7cfb30fc18fb9ba81cb9a71acdb28ef
Caro <fllcortizv@gmail.com>
2026-08-21 17:37:03 -0600

    Bandera de México

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -12,6 +12,7 @@ Console.Write("\n\n\n");
 // Generar un programa que muestre atributos en tabla con borde
 Console.Write("Estadísticas en forma de tabla");
 Console.Write("\n\n");
+
 Console.WriteLine("/ Greg \"El Mago\" /");
 Console.WriteLine("\\----------------\\");
 Console.WriteLine("/ HP\t100      /");
@@ -19,3 +20,26 @@ Console.WriteLine("\\ MP\t50       \\");
 Console.WriteLine("/ Gold\t1,500    /");
 Console.WriteLine("\\ Exp\t200      \\");
 
+Console.Write("\n\n\n");
+// Bandera de México con color
+Console.WriteLine("Bandera de México");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.Write("ME");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("XI");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("CO");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ME");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("XI");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("CO");
+
+Console.ResetColor();
