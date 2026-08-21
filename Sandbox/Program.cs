9fff583d7441c0a07540bdd5e24110190d68f3f0
Caro <fllcortizv@gmail.com>
2026-08-21 16:29:08 -0600

    Ejemplo de Secuencias de Escape

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,10 +4,16 @@ Console.WriteLine("¡Hola, Carolina!");
 Console.WriteLine("¡Hola, Carolina!");
 Console.WriteLine("¡Hola, Carolina!");
 
-Console.Write("Hola");
+Console.Write("¡Hola ");
 Console.Write("Carolina");
 Console.Write("!");
 
-
-
+Console.Write("\n\n\n");
+// Generar un programa que muestre atributos en tabla con borde
+Console.Write("Estadísticas en forma de tabla");
+Console.Write("\n\n");
+Console.WriteLine("/ Greg \"El Mago\" /");
+Console.WriteLine("\\----------------\\");
+Console.WriteLine("/ HP\t100      /");
+Console.WriteLine("\\ MP\t50       \\");
 
