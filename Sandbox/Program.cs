a9a2ec7b9add2fc77fc7104b56fc43487af32ca5
Caro <fllcortizv@gmail.com>
2026-08-21 18:20:40 -0600

    Ejercicio: Banderas de 5 Países

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -43,3 +43,117 @@ Console.BackgroundColor = ConsoleColor.Red;
 Console.Write("CO");
 
 Console.ResetColor();
+
+Console.Write("\n\n\n");
+// EJERCICIO: Banderas de 5 países
+Console.WriteLine("Banderas de 5 países");
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.Write("FR");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("AN");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("CIA");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("FR");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("AN");
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("CIA");
+
+Console.ResetColor();
+Console.WriteLine();
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ALE");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("MA");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("NIA");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("ALE");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("MA");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("NIA");
+
+Console.ResetColor();
+Console.WriteLine();
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("ES");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("PA");
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("ÑA");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("ES");
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.Write("PA");
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.Write("ÑA");
+
+Console.ResetColor();
+Console.WriteLine();
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("SU");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("I");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("ZA");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("SU");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("I");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("ZA");
+
+Console.ResetColor();
+Console.WriteLine();
+Console.Write("\n");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("IT");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("AL");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("IA");
+
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("IT");
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("AL");
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("IA");
+
+Console.ResetColor();
