52f5ce898ff1593b8a99c321d56ddbca88972222
Caro <fllcortizv@gmail.com>
2026-08-22 22:13:19 -0600

    Ejercicio: Variables y Multiplicaciones

--- Variables y Tipos de Datos/Program.cs
+++ Variables y Tipos de Datos/Program.cs
@@ -14,3 +14,31 @@ Console.WriteLine("Máximo: " + maxValorSbyte);
 // Símbolo de multiplicación: *
 // Símbolo de división: /
 
+Console.WriteLine ("\n");
+//Ejercicio: Utilizar dos tipos de datos diferentes al anterior
+// Uno entero y otro decimal, y mostrar sus valores máximos y mínimos
+// 1. Mostrar los valores min y max.
+// 2. Multiplicar el valor máximo por 10 y mostrar resultados.
+// 3. Multiplicar el valor minimo por 10 y mostrar resultados.
+
+// sbyte, byte, short, ushort, int, uint, long, ulong
+// float, double, decimal
+
+byte minValorByte = Byte.MinValue;
+byte maxValorByte = Byte.MaxValue;
+
+Console.WriteLine("Byte:");
+Console.WriteLine("Minimo: " + minValorByte);
+Console.WriteLine("Valor minimo * 10 = " + minValorByte * 10);
+Console.WriteLine("Maximo: " + maxValorByte);
+Console.WriteLine("Valor maximo * 10 = " + maxValorByte * 10);
+
+Console.WriteLine("\n");
+float minValorfloat = float.MinValue;
+float maxValorfloat = float.MaxValue;
+
+Console.WriteLine("Float:");
+Console.WriteLine("Minimo: " + minValorfloat);
+Console.WriteLine("Valor minimo * 10 = " + minValorfloat * 10);
+Console.WriteLine("Maximo: " + maxValorfloat);
+Console.WriteLine("Valor maximo * 10 = " + maxValorfloat * 10);
