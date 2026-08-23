6bfb7e6a6ecc3bad7fa397813a4579bd283ea23c
Caro <fllcortizv@gmail.com>
2026-08-22 21:52:34 -0600

    Ejemplo de Mostrar Valores Mínimos

--- Variables y Tipos de Datos/Program.cs
+++ Variables y Tipos de Datos/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Ejemplo: Obtener y mostrar los valores mínimos y máximos de un tipo de dato.
+
+sbyte minValorSbyte = SByte.MinValue;
+sbyte maxValorSbyte = SByte.MaxValue;
+
+Console.WriteLine("SByte");
+Console.WriteLine("Mínimo: " + minValorSbyte);
+Console.WriteLine("Máximo: " + maxValorSbyte);
+
+// Símbolo de suma: +
+// Símbolo de resta: -
+// Símbolo de multiplicación: *
+// Símbolo de división: /
+
