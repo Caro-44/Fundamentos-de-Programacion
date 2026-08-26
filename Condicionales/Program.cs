add55bc35be4010f77a294919191ccafe88b24fd
Caro <fllcortizv@gmail.com>
2026-08-26 10:43:55 -0600

    Condicionales

---
+++ Condicionales/Program.cs
@@ -0,0 +1,20 @@
+﻿//Situación de Condicionales - Ejercicio
+//DESARROLLAR un problema en el que se puedan utilizar condicionales
+
+//CONDICIONAL EXCLUYENTE:
+//Si tengo tres horas para jugar, juego The Witcher III, si tengo dos horas, juego Dead by Midnight
+//Si tengo una hora, Roblox, si tengo 30 minutos, Plato.
+
+//CONDICIONAL ANIDADA:
+//Acceso a la consola (con contraseña), si accede: elección de juego (1 & 2), Si no, no puede accesar.
+//Si escoge Juego 1: Elección de mundos.
+//Si escoge Juego 2: Elección de personaje.
+
+//Crear un programa que da una calificación y determine si es o no aprobatoria
+
+var calificacion = 10;
+
+if (calificacion>= 6) {Console.Write("Calificación aprobatoria");}
+else
+{Console.Write("Calificación reprobatoria");}
+
