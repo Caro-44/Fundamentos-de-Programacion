2eb87e59c153c97213c77dd10f38c647bf3926b1
Caro <fllcortizv@gmail.com>
2026-09-09 11:12:21 -0600

    Simulacro de Examen - Ejercicios

---
+++ Simulacro de Examen/Program.cs
@@ -0,0 +1,46 @@
+﻿//SIMULACRO DE EXAMEN
+
+//EJERCICIO 4
+
+//Crea un programa que pida al usuario dos valores. Verifica si son divisibles de forma exacta.
+//Un número es divisible entre otro de forma entera si la división no genera un residuo.
+
+Console.WriteLine("Escriba el divisor");
+float divisor= float.Parse(Console.ReadLine());
+
+Console.WriteLine("Escriba el dividendo");
+float dividendo= float.Parse(Console.ReadLine());
+
+if (divisor % dividendo == 0) Console.WriteLine("La división es exacta");
+else Console.WriteLine("La división no es exacta");
+
+//EJERCICIO 4
+
+// En un videojuego, una tienda aplica un impuesto especial al precio de los objetos que compra el jugador
+// Los primeros 20 créditos del precio del objeto no generan impuesto.
+// Los siguientes 20 créditos tienen un impuesto del 30%.
+// El resto del precio tiene un impuesto del 40%.
+
+//Genera un programa que solicite el precio base de un objeto y calcule el precio total que deberá pagar el
+//jugador, incluyendo el impuesto correspondiente.
+
+//ANÁLISIS DE PROBLEMA
+//Problema: Un videojuego aplica un impuesto especial al precio de los objetos.
+//Generar un programa que solicite el precio de un objeto y calcule el precio a pagar del jugador, con impuesto
+//EMTRADAS:
+//Precio objeto
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
